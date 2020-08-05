75d313ff6fee761beec1b9eefb33b8b7b95dfffa
SgoSkz <seangotskillz@gmail.com>
2020-08-04 22:02:50 -0500

    practice

--- pyCurses/attempt1.py
+++ pyCurses/attempt1.py
@@ -16,3 +16,48 @@ Learning curses.py through experience
         - Information
         - Images
 """
+
+import curses
+
+class MainWindow(object):
+    def __init__(self):
+        self.description = None
+        self.image = None
+        self.search = None
+        self.list = None
+
+    def setup_curses(self):
+        self.std = curses.initscr()
+
+    def initialize(self):
+        self.std.leaveok(0)
+        self.std.keypad(1)
+        curses.nocbreak()
+        curses.noecho()
+        curses.halfdelay(20)
+        curses.start_color()
+
+    def key_handler(self):
+        pass
+
+    def key(self):
+        pass
+
+    def selector(self):
+        pass
+
+    def update_size(self):
+        pass
+
+    @staticmethod
+    def close_window():
+        curses.endwin()
+
+
+def main():
+    windowMain = MainWindow()
+    windowMain.new_window()
+    windowMain.close_window()
+
+if __name__ == "__main__":
+    main()
